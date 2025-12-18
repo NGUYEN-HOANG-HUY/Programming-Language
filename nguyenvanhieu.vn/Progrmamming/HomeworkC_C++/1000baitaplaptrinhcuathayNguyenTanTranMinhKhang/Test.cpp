166c9ed010c6921ab11cce32a5edd7ffa92babd1
Nguyen Hoang Huy <nguyenhuyhoang2k4smart@gmail.com>
2025-12-18 21:11:06 +0700

    Initial commit

--- nguyenvanhieu.vn/Progrmamming/HomeworkC_C++/1000baitaplaptrinhcuathayNguyenTanTranMinhKhang/Test.cpp
+++ nguyenvanhieu.vn/Progrmamming/HomeworkC_C++/1000baitaplaptrinhcuathayNguyenTanTranMinhKhang/Test.cpp
@@ -1,44 +1,26 @@
-#include <stdio.h>
-#define MAX 1000
+#include <iostream>
 
-void nhap(int *n, int *m) {
-    do {
-        scanf("%d%d", n, m);
-        if (*n < 1 || *n > MAX || *m < 1 || *m > MAX) {
-            printf("Khong hop le, xin vui long nhap!\n");
-        }
-    } while (*n < 1 || *n > MAX || *m < 1 || *m > MAX);
-}
+using namespace std;
 
-void nhapMang(long long a[][MAX], int n, int m) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            scanf("%lld", &a[i][j]);
-        }
-    }
-}
-
-void xuatMang(long long a[][MAX], int n, int m) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            printf("Xuat mang vua nhap a[%d][%d]: %lld ", i, j, a[i][j]);
-        }
-        printf("\n");
-    }
+void nhapName(string &name) {
+    cin >> name;
 }
 
 int main() {
-    int T;
-    scanf("%d", &T);
-
-    while (T--) {
-        int n, m;
-        long long a[MAX][MAX];
-
-        nhap(&n, &m);
-        nhapMang(a, n, m);
-        xuatMang(a, n, m);
+    int t;
+    cin >> t;
+    while (t>0) {
+        string name;
+        nhapName(name);
+        cout << "Hello " + string(name);
+        cout << endl;
+        t -= 1;
     }
-
     return 0;
 }
+
+
+
+
+
+
